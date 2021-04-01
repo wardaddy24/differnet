b4e51ba5121c937d596183b38c45bc2ccd8ce35a
Aman Rastogi <40149518+wardaddy24@users.noreply.github.com>
2021-04-01 12:42:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import config as c
 from train import train
 from utils import load_datasets, make_dataloaders
 
+print("Started Running the Main File.....")
 train_set, test_set = load_datasets(c.dataset_path, c.class_name)
 train_loader, test_loader = make_dataloaders(train_set, test_set)
+print("Calling the train file......")
 model = train(train_loader, test_loader)
